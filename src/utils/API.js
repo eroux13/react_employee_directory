807460974e27f84ec966eea5321e8a51fce5b184
EJ <ej.roux13@gmail.com>
2021-05-31 10:07:43 -0700

    created file for axios API call

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+// Import axios for API call
+import axios from "axios";
+// API url
+const BASEURL = "https://randomuser.me/api/?nat=us&results=100&inc=gender,name,email,dob,phone,id,picture"
+
+// Axios API call
+export default {
+    getUsers: function() {
+        return axios.get(BASEURL);
+    }
+};
