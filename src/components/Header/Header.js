a16e4f6afab62531062d7513321db1cc5da8e0fa
EJ <ej.roux13@gmail.com>
2021-05-31 21:52:35 -0700

    created Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+// Import React
+import React from "react";
+// Import stylesheet
+import "./Header.js";
+
+function Header() {
+    return (
+        <div className="header display-5">Employee Directory</div>
+    );
+};
+
+export default Header;
