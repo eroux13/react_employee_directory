d1548bd2ac3616aeddda1aee404e98aac30fb45c
EJ <ej.roux13@gmail.com>
2021-06-02 22:53:21 -0700

    created Searchbar Component

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,21 @@
+// Import React
+import React from "react";
+// Import Stylesheet
+import "./Searchbar.css";
+
+function Searchbar(props){
+    return (
+        <form>
+            <input 
+                value={props.search}
+                name="search"
+                type="text"
+                placeholder="Search Employees"
+                onChange={props.handleInputChange}
+            />
+            <button className="btn btn-primary" onClick={props.handleFromSubmit}>Search</button>
+        </form>
+    )
+}
+
+export default Searchbar;
